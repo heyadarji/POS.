5bbe4d1ae2dd25576d5995bdba5477e2d71176e7
heyadarji <96716153+heyadarji@users.noreply.github.com>
2025-04-25 00:05:36 +0530

    updatee

--- omnipay/settings.py
+++ omnipay/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(ib^i^-3gvyyzsjw6q7x6w9%h00kn4r8u5fg_vuob8((j^ruya"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fasle
+DEBUG = False
 
 ALLOWED_HOSTS = ['omnipayweb.herokuapp.com', 'localhost', 'pos-app-607x.onrender.com', '127.0.0.1']
 
